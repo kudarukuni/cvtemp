2c0626e80c050493ea3ee0c2eb4038556e777e17
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 05:52:38 +0200

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,9 +7,6 @@ const Social = () => {
             <a href="https://twitter.com/naswillow" className="button" target="_blank">Twitter</a>
             <a href="https://www.linkedin.com/kudarukuni" className="button" target="_blank">LinkedIn</a>
             <a href="https://github.com/kudarukuni" className="button" target="_blank">GitHub</a>
-            <a href="https://api.whatsapp.com/send?phone=00263782516263" class="float" target="_blank">
-            <i class="fa fa-whatsapp my-float"></i>
-</a>
         </div>
     );
 }
