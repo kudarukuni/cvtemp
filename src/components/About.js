db5d86e5ac20e6cf5914fd208c08be31dc0e53c1
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 04:27:19 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,12 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Welcome to my 2D Online Curicullum Vitae, I'm Kuda a patient-focused 
+                Welcome to my 2D Online Curriculum Vitae, I am Kuda a patient-focused 
                 and empathic Software Engineer with 6 years of experience in Java, PHP, React, HTML,
-                CSS, Android Studio, C++ and JavaScript. A dynamic, innovative 
-                and results-driven individual with a great passion for success. Can
-                work under minimum supervision
-                A compassionate and hardworking developer looking
-                for a position in a small or major, rural or urban company
-                that may require such skills!
+                CSS, Android Studio, C++ and JavaScript. A dynamic, innovative and results-driven 
+                individual with a great passion for success. Can work under minimum supervision. 
+                A compassionate and hardworking developer looking for a position in a small or major, 
+                rural or urban company that may require such skills!
             </p>
         </section>
     );
