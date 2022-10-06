69348180d5937d1bce84ef19ccc2db70865a42dd
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 06:32:31 +0200

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,7 @@ const Experience = () => {
 
             <ul>
                 <li>
-                    <i>Visitation Makumbi High School</i> - <strong>O'Level & A'Level</strong>, 2014-2018
+                    <i><font color="#12bdf3;">Visitation Makumbi High School</font></i> - <strong>O'Level & A'Level</strong>, January 2011 - December 2013
                     <ul>
                         <li>Graduated with 8 O'Level Passes</li>
                         <li>Graduated with 1 HEXCO Computer Operations & Pacakage Passes</li>
@@ -13,34 +13,34 @@ const Experience = () => {
                     </ul>
                 </li>
                 <li>
-                    <i>Catholic University of Zimbabwe</i> <strong>Under Graduate Study</strong>, 2014-2018
+                    <i><font color="#12bdf3;">Catholic University of Zimbabwe</font></i> - <strong>Under Graduate Study</strong>, January 2014 - December 2018
                     <ul>
                         <li>Graduated with 2.1 (Upper Second Class)</li>
                     </ul>
                 </li>
                 <li>
-                    <i>ZESA Holdings</i> - <strong>Internship</strong>, January 2019 - December 2019
+                    <i><font color="#12bdf3;">ZESA Holdings</font></i> - <strong>Internship</strong>, January 2019 - December 2019
                     <ul>
                         <li>Studied Harware and Networking</li>
                         <li>Assisted Research &amp; Development on Web3 Products</li>
                     </ul>
                 </li>
                 <li>
-                    <i>Shungu DzeVana Childrens Home</i> - <strong>Voluntary</strong>, August 2019 - December 2019
+                    <i><font color="#12bdf3;">Shungu DzeVana Childrens Home</font></i> - <strong>Voluntary</strong>, August 2019 - December 2019
                     <ul>
                         <li>Tutoring Opharns</li>
                         <li>Assisted Research &amp; Development on Institutional IoT Security Products for Opharns</li>
                     </ul>
                 </li>
                 <li>
-                    <i>Mangwana Private Limited</i> - <strong>Post Graduate Study</strong>, January 2020 - December 2020
+                    <i><font color="#12bdf3;">Mangwana Private Limited</font></i> - <strong>Post Graduate Study</strong>, January 2020 - December 2020
                     <ul>
                         <li>Blockchain Development and Smart Contract Writting</li>
                         <li>Assisted Research and Development on Web3.0 Use Cases On the Ethereum &amp; Solana EcoSystem</li>
                     </ul>
                 </li>
                 <li>
-                    <i>Netcast System</i> - <strong>Tender Contractor</strong>, June 2021 - today
+                    <i><font color="#12bdf3;">Netcast System</font></i> - <strong>Tender Contractor</strong>, June 2021 - today
                     <ul>
                         <li>Senior Software Engineer or Full Stack Developer</li>
                         <li>Contributed to Smart Boardroom Solutions using Internet of Things.</li>
