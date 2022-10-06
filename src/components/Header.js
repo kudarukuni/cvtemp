e0ba067275267326fc87e3da2d6d133f6b67e85a
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-10-06 04:31:00 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
             <img src={profile} alt="Kudakwashe William Rukuni" />
 
             <div className='header__content'>
-                <h1>Hi, I am Kudakwashe William Rukuni</h1>
-                <p>Full Stack Web3 Developer</p><br/>
-                <a href="https://contactkuda-kudarukuni.vercel.app" className="button" target="_blank">Get In Touch</a><br/><br/><br/>
+                <h1>Hi, I am Kudakwashe William Rukuni</h1><br/><br/>
+                <p>Full Stack Web3 Developer</p><br/><br/>
+                <a href="https://contactkuda-kudarukuni.vercel.app" className="button" target="_blank">Get In Touch</a><br/><br/>
                 <a href="https://kudarukuni-kudarukuni.vercel.app" className="button" target="_blank">Desktop Site</a>
             </div>
         </section>
